934a03db906e11f35d32dfaaf1f5ba5763546afa
yjin0295 <yjin0295@uni.sydney.edu.au>
2023-10-28 19:17:06 +1100

    divide class

--- sketch.js
+++ sketch.js
@@ -1,177 +1,142 @@
-let circles = []; //use array to store all the circles we have
-const maxRadius = 40; //the maximum radius for our smaller circle inside the big one
-const bigCircleRadius = 70; //set the big circle radius
+// array to store all circle objects
+let circles = [];
+const maxRadius = 40; // maximum radius for smaller circles
+const bigCircleRadius = 70; // radius of big circle
 
-
-//set up the initial things
 function setup() {
- createCanvas(windowWidth, windowHeight); //create a canvas, using window height and width
- colorMode(HSB); //set the color mode to HSB to let us easy set the brightness and saturation, get a better color
- makeCircles();  //call the function to create our circle.
+createCanvas(windowWidth, windowHeight); //create a canvas that covers the entire browser
+colorMode(HSB); // set color mode to Hue Saturation Brightness
+makeCircles(); // call function to create circle objects
 }
 
-
-//create a function to draw our circles
+// function to create and store circle objects
 function makeCircles() {
- circles = []; //clear existing circles
+circles = []; // clear the array
+// calculate number of columns and rows for big circles based on the canvas width and height
+let cols = floor(width / (bigCircleRadius * 2 + 5));
+let rows = floor(height / (bigCircleRadius * 2 + 5));
 
+// nested loops to create circle objects for each column and row
+for (let row = 0; row < rows; row++) {
+for (let col = 0; col < cols; col++) {
+// calculate position for the big circle
+let x = (bigCircleRadius + 2.5) + col * (bigCircleRadius * 2 + 5);
+let y = (bigCircleRadius + 2.5) + row * (bigCircleRadius * 2 + 5);
+let hue = random(360); // random hue for big circle color
+circles.push(new BigCircle(x, y, bigCircleRadius, color(hue, 5, 90))); // create big circle object and add to array
 
- //calculate the number of columns and rows according to our big circle's radius and space
- let cols = floor(width / (bigCircleRadius * 2 + 5)); //columns in canvas
- let rows = floor(height / (bigCircleRadius * 2 + 5)); //rows in canvas
-
-
- //loop over each row and column to create circles.
- for (let row = 0; row < rows; row++) {
-   for (let col = 0; col < cols; col++) {
-    
-     //calculate position of each big circle
-     let x = (bigCircleRadius + 2.5) + col * (bigCircleRadius * 2 + 5); //x-coordinate
-     let y = (bigCircleRadius + 2.5) + row * (bigCircleRadius * 2 + 5); //y-coordinate
-     let hue = random(360); //pick a random color for our big circle
-
-
-     //create a big circle and add it to the array
-     circles.push(new Circle(x, y, bigCircleRadius, color(hue, 5, 90), true));
-
-
-     //loop to create 6 smaller circles within each large circle
-     for (let j = 0; j < 6; j++) {
-
-
-       let brightness = 70; //set the brightness level for small circle
-       let reducedSaturation = 80; //set the saturation level for small circle
-       let hueSmall = random(360); //get a color for small circle
-       let shade = color(hueSmall, reducedSaturation, brightness, 0.7); //get a color with the defined hue, saturation, brightness, and let an opacity to 0.7.
-       //calculate the radius of the smaller circle.
-       //each consecutive circle will be smaller due to the factor (1 - j * 0.2).
-       let radius = maxRadius * (1.0) * (1 - j * 0.2) * 0.9;
-       //make a new Circle object with the calculated properties and add it to the 'circles' array.
-       //'False' argument signifies this is not one of the main larger circles.
-
-
-       circles.push(new Circle(x, y, radius, shade, false));
-     }
-
-
-     //make a really small circle in every big circle
-     let hueCenter = random(360); //pick a random color for our center circle
-     circles.push(new Circle(x, y, maxRadius * 0.2, color(hueCenter, 100, 50, 0.7), false));
-   }
- }
+// loop to create 6 small circles inside each big circle
+for (let j = 0; j < 6; j++) {
+let hueSmall = random(360); // random hue for small circle color
+let shade = color(hueSmall, 80, 70, 0.7); // create color with the random hue
+let radius = maxRadius * (1.0) * (1 - j * 0.2) * 0.9; // calculate radius for small circle
+circles.push(new SmallCircle(x, y, radius, shade)); // create small circle object and add to array
 }
 
+let hueCenter = random(360); // random hue for center dot color
+circles.push(new CircleCenter(x, y, maxRadius * 0.2, color(hueCenter, 100, 50, 0.7))); // create circle center object and add to array
+}
+}
+}
 
-//make a function to help us resize the canvas and create new circle while the windows change
 function windowResized() {
- resizeCanvas(windowWidth, windowHeight);
- makeCircles();
+resizeCanvas(windowWidth, windowHeight); // resize canvas to fit the window
+makeCircles(); // recreate circle objects to fit new window size
 }
-
 
 function draw() {
- background(44, 61, 100); //set our background color to light yellow using HSB color mode
- //loop through all the circles and show them
- for (let circle of circles) {
-   circle.show();
- }
-}
-
-
-//define our circle here
-class Circle {
- //use constructor here, initializes properties of the circle object
- constructor(x, y, radius, base, isBigCircle) {
-   this.pos = createVector(x, y); //give a position to x and y coordinate
-   this.base = base;  //store 'base' color to circle object 'base'
-   this.radius = radius; //store the size of the circle in the 'radius' property
-   this.isBigCircle = isBigCircle; //a boolean to check if it's a big circle
- }
-
-
- //method to show circle
- show() {
-   fill(this.base); //fill the color
-
-
-   //if it is a big circle, set the stroke color and stroke weight
-   if (this.isBigCircle) {
-
-
-     let strokeCol = color(hue(this.base), 90, brightness(this.base) - 60);
-     stroke(strokeCol);
-     strokeWeight(1.5);
-
-
-   } else { //if it is not a big circle, small circle, the stroke will be white
-     stroke(255); //
-     strokeWeight(0.5); //set the stroke weight here
-   }
-
-
-   //draw ellipse
-   ellipse(this.pos.x, this.pos.y, this.radius * 2);
-
-
-//if it's a big circle, the draw some line between the area of big circle and the biggest small circle
-  if (this.isBigCircle) {
-    let numLines = 50;  //set the number of the line
-    let innerRadius = maxRadius * 0.9;  //set the biggest inner circle's radius
-    stroke(34, 100, 100);  //set the color of the lines
-
-
-    for (let i = 0; i < numLines; i++) {
-
-
-      let angle = TWO_PI / numLines * i;//calculate the angle
-      let innerX = this.pos.x + cos(angle) * innerRadius;
-      let innerY = this.pos.y + sin(angle) * innerRadius;
-      let outerX = this.pos.x + cos(angle) * this.radius;
-      let outerY = this.pos.y + sin(angle) * this.radius;
-      line(innerX, innerY, outerX, outerY);  //draw lines
-    }
-  }
- //if it's not a big circle, draw additional details
- if (!this.isBigCircle) {
-
-
-   //draw inner circle, smaller circles, and an outer circle
-   let innerRadius = this.radius * 0.5;
-   ellipse(this.pos.x, this.pos.y, innerRadius * 2);//draw an ellipse at the center with the calculated inner radius to create an inner circle
-
-
-
-
-   for (let j = 0; j < 6; j++) {
-     //outer loop, iterating from 0 to 5,create patterns in six different directions
-     for (let i = 0; i < 24; i++) {
-
-
-       //inner loop, iterating from 0 to 23, create a series of small ellipses within each direction
-       let angle = TWO_PI / 24 * i + j * PI / 12; //calculate the angle to evenly space the small ellipses in the current direction
-       let xOffset = cos(angle) * this.radius * 0.7; //calculate the horizontal offset using the cosine of the angle
-       let yOffset = sin(angle) * this.radius * 0.7; //calculate the vertical offset using the sine of the angle
-       //draw a small ellipse at the adjusted position to create a detailed pattern within the direction
-       ellipse(this.pos.x + xOffset, this.pos.y + yOffset, this.radius * 0.15);
-     }
-   }
-   //loop from 0 to 4 (five times) to create a set of ellipses
-   for (let i = 0; i < 5; i++) {
-
-
-     let angle = TWO_PI / 5 * i; //calculate the angle for evenly spacing the ellipses in a circle
-     let xOffset = cos(angle) * (this.radius * 2.5); //calculate the horizontal offset based on the cosine of the angle
-     let yOffset = sin(angle) * (this.radius * 2.5); //calculate the vertical offset based on the sine of the angle
-     ellipse(this.pos.x + xOffset, this.pos.y + yOffset, this.radius * 0.2);// draw an ellipse at the adjusted position to create a decorative pattern
-
-
-   }
- }
+background(44, 61, 100); // set background color
+// loop through all circle objects and call their show method
+for (let circle of circles) {
+circle.show();
 }
 }
-    
-    
-    
-    
-    
+
+class BigCircle {
+// constructor for creating big circle object
+constructor(x, y, radius, base) {
+this.pos = createVector(x, y); // store position as a vector
+this.base = base; // store color
+this.radius = radius; // store radius
+}
+
+// method to display the big circle
+show() {
+fill(this.base); // set fill color
+stroke(color(hue(this.base), 90, brightness(this.base) - 60)); // set stroke color
+strokeWeight(1.5); // set stroke weight
+ellipse(this.pos.x, this.pos.y, this.radius * 2); // draw the big circle
+let numLines = 50; // number of lines to draw inside big circle
+let innerRadius = maxRadius * 0.9; // calculate inner radius for lines
+stroke(34, 100, 100); // set color for lines
+
+// loop to draw lines inside the big circle
+for (let i = 0; i < numLines; i++) {
+let angle = TWO_PI / numLines * i; // calculate angle for each line
+let innerX = this.pos.x + cos(angle) * innerRadius; // calculate starting x position for line
+let outerX = this.pos.x + cos(angle) * this.radius; // calculate ending x position for line
+let innerY = this.pos.y + sin(angle) * innerRadius; // calculate starting y position for line
+let outerY = this.pos.y + sin(angle) * this.radius; // calculate ending y position for line
+line(innerX, innerY, outerX, outerY); // draw the line
+}
+}
+}
+
+// class for the small circle
+class SmallCircle {
+// constructor for creating small circle object
+constructor(x, y, radius, base) {
+this.pos = createVector(x, y); // store position as a vector
+this.base = base; // store color
+this.radius = radius; // store radius
+}
+
+// method to display the small circle
+show() {
+fill(this.base); // set fill color
+stroke(255); // set stroke color to white
+strokeWeight(0.5); // set stroke weight
+ellipse(this.pos.x, this.pos.y, this.radius * 2); // draw the small circle
+let innerRadius = this.radius * 0.5; // calculate inner radius for smaller circle inside
+ellipse(this.pos.x, this.pos.y, innerRadius * 2); // draw smaller circle inside
+
+// nested loops to draw dots around the small circle
+for (let j = 0; j < 6; j++) {
+for (let i = 0; i < 24; i++) {
+let angle = TWO_PI / 24 * i + j * PI / 12; // calculate angle for each dot
+let xOffset = cos(angle) * this.radius * 0.7; // calculate x offset for dot position
+let yOffset = sin(angle) * this.radius * 0.7; // calculate y offset for dot position
+ellipse(this.pos.x + xOffset, this.pos.y + yOffset, this.radius * 0.15); // draw the dot
+}
+}
+
+// loop to draw 5 dots around the small circle
+for (let i = 0; i < 5; i++) {
+let angle = TWO_PI / 5 * i; // calculate angle for each dot
+let xOffset = cos(angle) * (this.radius * 2.5); // calculate x offset for dot position
+let yOffset = sin(angle) * (this.radius * 2.5); // calculate y offset for dot position
+ellipse(this.pos.x + xOffset, this.pos.y + yOffset, this.radius * 0.2); // draw the dot
+}
+}
+}
+
+// class for the circle center of the small circle
+class CircleCenter {
+// constructor for creating center dot object
+constructor(x, y, radius, base) {
+this.pos = createVector(x, y); // store position as a vector
+this.base = base; // store color
+this.radius = radius; // store radius
+}
+
+show() {
+fill(this.base); // set fill color
+stroke(255); // set stroke color to white
+strokeWeight(0.5); // set stroke weight
+ellipse(this.pos.x, this.pos.y, this.radius * 2); // draw the circle center
+}
+}
+
+
+
 
